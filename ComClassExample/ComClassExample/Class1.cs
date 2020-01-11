34c5670eda5f0cb1164203319db0942f26396503
Adão Duque <adaoduquesn@gmail.com>
2020-01-11 19:41:45 -0300

    Commit source

---
+++ ComClassExample/ComClassExample/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ComClassExample
+{
+    [Guid("7068AC34-DBB0-4e40-84A7-C2243355E2D7"),
+    InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface IComClassExample
+    {
+        [DispId(1)]
+        int AddTheseUp(int adder1, int adder2);
+    }
+
+    [Guid("863AEADA-EE73-4f4a-ABC0-3FB384CB41AA"),
+    ClassInterface(ClassInterfaceType.None)]
+    public class ComClassExample : IComClassExample
+    {
+        // constructor - does nothing in this example
+        public ComClassExample() { }
+
+        // a method that returns an int
+        public int AddTheseUp(int adder1, int adder2)
+        {
+            return adder1 + adder2;
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 ###DLL Extension PHP write with C#
 
 ####Credits: (Whoisburiedhere)[https://whoisburiedhere.wordpress.com/2011/07/12/creating-a-com-object-from-scratch-with-c]
+
+
